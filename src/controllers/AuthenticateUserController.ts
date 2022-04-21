1fd40125f6b800b2c28a0cd9e782584c6de83d57
LucasRLemos <lucasrlemos03@gmail.com>
2022-04-20 22:33:11 -0300

    authentication added

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,7 +5,15 @@ class AuthenticateUserController{
     async handle(request: Request, response: Response){
         const service = new AuthenticateUserService();
     
-        //service.execute();
+        const {code} = request.body
+    
+        try{
+             const result = await service.execute(code);
+            return response.json(result);
+        } catch(err){
+            return response.json({ error: err.message });
+
+        }
     }
 }
 
